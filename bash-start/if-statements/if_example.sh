25f4bdb1174120e6db3f2a317e20602a46bad2dc
lisiurday <lisiurday@gmail.com>
2019-01-03 13:02:47 +0800

    feat: update

--- bash-start/if-statements/if_example.sh
+++ bash-start/if-statements/if_example.sh
@@ -4,3 +4,11 @@ if [ $1 -gt 100 ]
 then
   echo Hey that\'s a large number.
 fi
+
+if [ 8 -gt 2 ];then
+  echo 2==2
+fi
+
+if [ -n 'a' ];then
+  echo empty
+fi
---
+++ bash-start/if-statements/tempCodeRunnerFile.sh
@@ -0,0 +1,7 @@
+if [ 8 -gt 2 ];then
+#   echo 2==2
+# fi
+
+# if [ -n 'a' ];then
+#   echo empty
+# fi
